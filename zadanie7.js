6e883afd6ad640fbe5cdeb231172b1d38a1d2cfd
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-02-21 13:35:35 +0100

    fixed zad7

--- zadanie7.js
+++ zadanie7.js
@@ -1,8 +1,11 @@
 
 'use strict';
 let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3];
-
+let arr4 = []
 arr3.forEach(item => {
+  if(!arr4.includes(item)){ 
+    arr4.push(item)
   console.log(item + " wystepuje" + " " + arr3.filter(
     item2 => item2 === item).length + " razy");
+  }
 });
